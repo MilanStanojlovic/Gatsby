411e31b954c5d9506e1f163f6567481ed71e338d
Milan <mistikone1@gmail.com>
2020-03-29 15:53:50 +0200

    gatsby styling and scss modules

--- src/components/newLayout.js
+++ src/components/newLayout.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Footer from './footer';
 
+import layout from './newLayout.module.scss';
+import Header from './header';
+
 
 const NewLayout = (props) => {
   return (
-    <div>
+    <div className={layout.container}>
+      <Header siteTitle="About" />
+      <div className={layout.content}>
+        {props.children}
+      </div>
 
-      {props.children}
       <Footer />
     </div>
   )
